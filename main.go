1a3c41ce8cab5f7076a38f43b5ea3eed3d646a8d
Parallels <parallels@Praveens-MacBook-Air.local>
2022-10-15 12:57:14 +0530

    sample commit

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func main()  {
+	var age = 18
+	var name = "sughashini"
+
+	var nameOne string ="mario"
+	
+	var nameTwo = "good"
+
+	var nameThree string
+	fmt.Println(nameOne,nameTwo,nameThree)
+
+	nameOne = "peach"
+	nameThree = "browser"
+	
+	fmt.Println(nameOne,nameTwo,nameThree)
+	nameFour := "nice day"
+	fmt.Println(nameFour)
+
+	var ageOne = 10
+	var ageTwo = 30
+	var ageThree int =18
+	ageFour:= 17
+	fmt.Println("\n",ageOne,"\n",ageTwo,"\n",ageThree,"\n",ageFour)
+
+	fmt.Print("hello,")
+	fmt.Print("world! \n")
+    fmt.Print("newline \n")
+	fmt.Println("hello ninnjas!")
+	fmt.Println("goodbye ninjas!")
+	fmt.Println("my age is", age,"and my name is", name)
+
+}
